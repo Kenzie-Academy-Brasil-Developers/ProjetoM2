f9c1f833a89dee1d79a565456b846dc128ac6776
maksonss4 <maksonsantoss4@gmail.com>
2022-07-11 16:12:55 -0300

    feat: Criando branch develop

---
+++ src/controller/usuario.controller.js
@@ -0,0 +1,22 @@
+class Usuario {
+    static baseUrl = "https://habits-kenzie.herokuapp.com/api"
+
+    static login (data) {
+    
+    }
+
+    static createUser (data) {
+
+    }
+
+    static updateUser () {
+
+    }
+
+    static deleteUser () {
+
+    }
+}
+
+// E-mail: grupo3Caique@mail.com
+// Senha: 61532680c163157d92f094e6a0d4303f
