4f4c396da5d3ad4ffa5ce7f0ef3c6767cdf9a0cb
guilhermesilva <2003silvagui@gmail.com>
2022-07-12 18:13:10 -0300

    feat: adicionando evento de click para abrir modais

--- src/scripts/homePage.scripts.js
+++ src/scripts/homePage.scripts.js
@@ -1,5 +1,5 @@
 //evento de abrir e fechar modal criar habito
-const modal = document.querySelector('#modal_criarHabito')
+const modal = document.querySelector('#criarHabito')
 const buttonCriarModal = document.querySelector('.botaoCriar')
 buttonCriarModal.addEventListener('click', () => {
     modal.style.display = 'flex'
@@ -11,11 +11,28 @@ buttonFecharModal.addEventListener('click', () => {
 
 //evento de abrir e fechar modal de excluir habito
 const modalExcluir = document.querySelector('#modal_excluirHabito')
-const buttonExcluirModal = document.querySelector('')
+const buttonExcluirModal = document.querySelector('.botaoExcluir')
 buttonExcluirModal.addEventListener('click', () => {
+    modalEdit.style.display = 'none'
     modalExcluir.style.display = 'flex'
 })
 const buttonFecharModalExcluir = document.querySelector('.imageFecharExcluir')
 buttonFecharModalExcluir.addEventListener('click', () => {
     modalExcluir.style.display = 'none'
 })
+
+
+
+// evento para abrir o editar 
+const edit = document.querySelectorAll('.conteudoEditar')
+const modalEdit = document.querySelector('#modal_editarHabito')
+edit.forEach(elem => {
+    elem.addEventListener('click', () => {
+        modalEdit.style.display = 'flex'
+    })
+})
+const buttonFecharEdit = document.querySelector(".imageFecharEdit")
+buttonFecharEdit.addEventListener('click', () => {
+    modalEdit.style.display = 'none'
+})
+
