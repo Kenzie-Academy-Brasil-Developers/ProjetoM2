5a6f4a603d2d9b7ff8a15d08eb13a22305c3666b
Antonio Rodrigues <antoniovkuhl@gmail.com>
2022-07-12 15:47:31 -0300

    feat: criação das requisições de api

--- src/controller/usuario.controller.js
+++ src/controller/usuario.controller.js
@@ -1,21 +1,41 @@
-class Usuario {
+export default class Usuario {
     static baseUrl = "https://habits-kenzie.herokuapp.com/api"
+    static token = JSON.parse(localStorage.getItem("@kenzie-habits-token"))
 
-    static login (data) {
-    
+    static async login(data) {
+        return await fetch(`${this.baseUrl}/userLogin`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+            })
+            .then(res => res.json())
+            .then(res => {
+                if (res.token !== undefined) {
+                    localStorage.setItem("@kenzie-habits-token", JSON.stringify(res.token))
+                }
+                return res
+            })
+            .catch(err => console.log(err))
+
+
     }
 
-    static createUser (data) {
-
+    static async updateUser(data) {
+        return await fetch(`${this.baseUrl}/user/profile`, {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${this.token}`
+                },
+                body: JSON.stringify(data)
+            })
+            .then(res => res.json())
+            .then(res => res)
+            .catch(err => console.log(err))
     }
 
-    static updateUser () {
-
-    }
-
-    static deleteUser () {
-
-    }
 }
 
 // E-mail: grupo3Caique@mail.com
